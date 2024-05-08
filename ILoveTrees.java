518969ffb7d1a9281ee633402ccad364a2454b94
RuvicSeno <ainsmegunee@gmail.com>
2024-05-08 15:58:52 +0800

    update

---
+++ ILoveTrees.java
@@ -0,0 +1,10 @@
+public class ILoveTrees{
+    public static void main(String[] args) {
+
+        int repeat = 3;
+
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("I Love Trees");
+        }
+    }
+}
